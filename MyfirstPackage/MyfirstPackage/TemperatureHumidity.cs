ab07a6373ab8689410c710010e1e9afa9f64f9fd
batzoo <40250093+batzoo@users.noreply.github.com>
2018-06-14 12:56:58 +0200

    Premier Pkg
    Yo c le premier pkg
    C mon premier envoi les gars

---
+++ MyfirstPackage/MyfirstPackage/TemperatureHumidity.cs
@@ -0,0 +1,12 @@
+﻿using Constellation.Package;
+using System;
+
+namespace MyFirstPackage
+{
+    [StateObject]
+    public class TemperatureHumidity
+    {
+        public double Temperature { get; set; }
+        public int Humidity { get; set; }
+    }
+}
